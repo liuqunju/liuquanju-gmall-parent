41894d90e998eb79e88e6364d07e089671fecc79
liuquanju <yingjie.liu@outlook.com>
2020-01-05 17:14:17 +0800

    项目整合dubbo

---
+++ liuquanju-gmall-user-service/src/main/java/com/liuquanju/gmall/user/LiuquanjuGmallUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.liuquanju.gmall.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiuquanjuGmallUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiuquanjuGmallUserServiceApplication.class, args);
+    }
+
+}
---
+++ liuquanju-gmall-user-web/src/main/java/com/liuquanju/gmall/user/LiuquanjuGmallUserWebApplication.java
@@ -0,0 +1,13 @@
+package com.liuquanju.gmall.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiuquanjuGmallUserWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiuquanjuGmallUserWebApplication.class, args);
+    }
+
+}
